24d0283ac74a4af6a213645074618d75e4118c72
emineegnss <80625362+emineegnss@users.noreply.github.com>
2023-09-27 01:27:22 +0300

    26/09 Part/3

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AlertifyOptions,
+  AlertifyService,
+  MessageType,
+  Position,
+} from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
-
+export class DashboardComponent implements OnInit {
+  constructor(private alertifyService: AlertifyService) {}
+  ngOnInit(): void {}
+  m() {
+    this.alertifyService.message('Error', { messageType: MessageType.Error, delay: 2, position:Position.TopCenter,dismissOthers:true});
+  }
+  d() {
+    this.alertifyService.dismiss();
+  }
 }
--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-declare var alertify: any;
 
 @Component({
   selector: 'app-layout',
@@ -8,8 +7,8 @@ declare var alertify: any;
 })
 export class LayoutComponent implements OnInit {
   ngOnInit(): void {
-    alertify.alert("Ready");
-
+    throw new Error('Method not implemented.');
   }
+ 
 
 }
