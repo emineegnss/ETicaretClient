5632e772eb54a8af68f720ad108984596b750f29
emineegnss <80625362+emineegnss@users.noreply.github.com>
2023-09-27 18:28:46 +0300

    27/09 Part/1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CostomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/costom-toastr.service';
 declare var $: any
 @Component({
   selector: 'app-root',
@@ -7,6 +9,22 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaretClient';
+  constructor(private toastr:CostomToastrService){
+    toastr.message("Selam","Başarisiz",{
+      messageType : ToastrMessageType.Error,
+      position: ToastrPosition.TopLeft
+    })
+    toastr.message("Selam","Başarisiz",{
+      messageType : ToastrMessageType.Success,
+      position:  ToastrPosition.TopLeft})
+    toastr.message("Selam","Başarisiz",{
+      messageType : ToastrMessageType.Warning,
+      position:  ToastrPosition.TopFullWidth})
+    toastr.message("Selam","Başarisiz",{
+      messageType : ToastrMessageType.Info,
+      position:  ToastrPosition.BottomFullWidth})
+
+  }
 }
 // $(document).ready(()=>{
 //   alert("asd")
